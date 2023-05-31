49932ffdde7f222c8022a60891b65df74a293a43
최지연 <choi02647@naver.com>
2023-05-31 21:23:43 +0900

    added Category

---
+++ UploadUsedGoodsApp/Entities/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  UploadUsedGoodsApp
+//
+//  Created by Jiyeon Choi on 2023/05/31.
+//
+
+import Foundation
+
+struct Category {
+    let id: Int
+    let name: String
+}

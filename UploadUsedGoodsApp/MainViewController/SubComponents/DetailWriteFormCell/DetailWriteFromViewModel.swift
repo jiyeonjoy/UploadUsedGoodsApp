e5041f6001a2530bf8a36b5fb259a5c99f3f8b83
최지연 <choi02647@naver.com>
2023-05-31 22:00:09 +0900

    added DetailWriteFormCellViewModel, DetailWriteFormCell

---
+++ UploadUsedGoodsApp/MainViewController/SubComponents/DetailWriteFormCell/DetailWriteFromViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  DetailWriteFromViewModel.swift
+//  UploadUsedGoodsApp
+//
+//  Created by Jiyeon Choi on 2023/05/31.
+//
+
+import RxSwift
+import RxCocoa
+
+struct DetailWriteFormCellViewModel {
+    let contentValue = PublishRelay<String?>()
+}

05ae8beae0472e57db21a2208a17ce6a238d12f1
최지연 <choi02647@naver.com>
2023-05-31 20:59:56 +0900

    Initial Commit

---
+++ UploadUsedGoodsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UploadUsedGoodsApp
+//
+//  Created by Jiyeon Choi on 2023/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

445321b31a10cf42fbbb8a08f3a15f2202f0ac6d
최지연 <choi02647@naver.com>
2023-05-31 21:22:36 +0900

    added TitleTextFieldCell, TitleTextFieldViewModel

---
+++ UploadUsedGoodsApp/MainViewController/SubComponents/TitleTextFieldCell/TitleTextFieldViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitleTextFieldViewModel.swift
+//  UploadUsedGoodsApp
+//
+//  Created by Jiyeon Choi on 2023/05/31.
+//
+
+import RxCocoa
+
+struct TitleTextFieldViewModel {
+    let titleText = PublishRelay<String?>()
+}
